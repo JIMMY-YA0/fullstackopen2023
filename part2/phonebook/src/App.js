5ff87eb52f0cc68ff52cdd493cc6e829486e66b4
Jimmy Yao <jimmyyaocs@gmail.com>
2023-02-27 23:12:11 +1100

    Part2 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,17 +3,18 @@ import Search from "./components/Search";
 import AddPerson from "./components/AddPerson";
 import Filter from "./components/Filter";
 import PersonList from "./components/PersonList";
+import { useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => setPersons(response.data));
+  }, []);
 
   const submitHandler = (event) => {
     event.preventDefault();
