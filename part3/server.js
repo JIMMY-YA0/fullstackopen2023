28db145f08d507345306d0159b193d404f45be64
Jimmy Yao <jimmyyaocs@gmail.com>
2023-03-01 15:43:09 +1100

    Part3 3.5-3.6

--- part3/server.js
+++ part3/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let personData = [
   {
     id: 1,
@@ -51,7 +53,33 @@ app.get("/api/person/:id", (req, res) => {
 app.delete("/api/person/:id", (req, res) => {
   const id = Number(req.params.id);
   const newPersonData = personData.filter((person) => person.id !== id);
-  res.json(newPersonData);
+  res.status(204).end();
+});
+
+const generatedId = () => {
+  return Math.floor(Math.random() * 30000);
+};
+
+app.post("/api/person", (req, res) => {
+  const body = req.body;
+  console.log("log", body);
+  const existName = personData.find(
+    (person) => person.name.toLowerCase() === body.name.trim().toLowerCase()
+  );
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: "The name or number is missing" });
+  } else if (existName) {
+    return res.status(400).json({ error: "Name must be unique" });
+  }
+
+  const newPerson = {
+    id: generatedId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = personData.concat(newPerson);
+  res.json(persons);
 });
 
 const PORT = 3001;
